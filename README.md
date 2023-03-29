861fce925a5fb25b59bf8209a85577560d86038c
classabbyamp <dev@placeviolette.net>
2023-03-29 00:49:47 -0400

    TMP

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
-# sundiald
+# sundial
 
-[`org.freedesktop.timedate1`](https://www.freedesktop.org/software/systemd/man/org.freedesktop.timedate1.html) dbus daemon for non-systemd systems
+[`org.freedesktop.timedate1`](https://www.freedesktop.org/software/systemd/man/org.freedesktop.timedate1.html) D-Bus daemon and client for non-systemd systems.
+`org.freedesktop.timedate1` is the D-Bus interface used by systemd's `timedated` and `timedatectl`.
+Many desktop environments also use this D-Bus interface as the backend for their time and date settings UIs:
+
+| Desktop Environment | Uses `timedate1` D-Bus Interface? | `sundial` Tested? | Notes |
+|:--------------------|:---------------------------------:|:-----------------:|:------|
+| Budgie       | ✅ | ❔ | |
+| Cinnamon     | ✅ | ❔ | |
+| Cutefish     | ✅ | ❔ | |
+| Deepin       | ✅ | ❔ | has an alternative interface that does nothing (set at build-time) |
+| GNOME 3      | ✅ | ❔ | |
+| KDE Plasma 5 | 🟨 | ❔ | has "legacy" methods to fall back to if the D-Bus interface is unavailable |
+| LXDE         | ❌ | -  | |
+| LXQt         | ✅ | ❔ | |
+| MATE         | ✅ | ❔ | |
+| Pantheon     | ✅ | ❔ | |
+| Xfce         | ❌ | -  | |
+
+> ***Key:** ✅: Required/Works, 🟨: Optional, ❌: Not used/Does not work, ❔: Untested*
+
+**Required:**
+
+- Budgie
+- Cinnamon
+- GNOME
+
+**Optional:**
+
+- KDE Plasma (has "legacy" methods to fall back to if the D-Bus interface is unavailable)
+- 
+
+## Installation
+
+with [rinstall](https://github.com/danyspin97/rinstall):
+```
+# rinstall install --system
+# rinstall install --system --yes
+```
+
+## Usage
+
+`sundiald` will be launched automatically when requested over D-Bus.
+`sundialctl` is a small utility to interface with `sundiald` from the command-line.
+See `sundialctl --help` for more information.
+
+## Copyright
+
+Copyright (c) 2023, classabbyamp  
+Released under the BSD-2-Clause licence
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::{path::Path, os::fd::RawFd};
 
+use enumflags2::BitFlag;
 use nix::errno::Errno;
 use tokio::fs::canonicalize;
 use zbus::{dbus_interface, Connection, fdo};
+use zbus_polkit::policykit1::{AuthorityProxy, Subject, CheckAuthorizationFlags};
 
 use util::{rtc_open, rtc_read, read_lines};
 
@@ -15,15 +17,19 @@ const MAX_PHASE: libc::c_long = 500_000_000;
 struct TimeDate {
     tz: Option<String>,
     rtc: Result<RawFd, Errno>,
+    auth: AuthorityProxy<'static>,
+    subject: Subject,
 }
 
 #[dbus_interface(name = "org.freedesktop.timedate1")]
 impl TimeDate {
     /// change the system clock
-    #[allow(unused_variables)]
     async fn set_time(&self, usec_utc: i64, relative: bool, interactive: bool) -> fdo::Result<()> {
         // TODO
         // https://github.com/systemd/systemd/blob/main/src/timedate/timedated.c#L820
+
+        // get a starting timestamp now
+
         // TODO: if ntp enabled, fail
 
         if relative {
@@ -31,6 +37,9 @@ impl TimeDate {
                 return Ok(());
             }
 
+            // get now()
+            // now + usec_utc
+            // ensure no overflow/underflow
 
         } else {
             if usec_utc <= 0 {
@@ -38,23 +47,53 @@ impl TimeDate {
             }
         }
 
+        // polkit verify
+        self.check_auth("org.freedesktop.timedate1.set-time", interactive).await?;
+        // adjust for time spent: add now - starting timestamp
+        // set system clock
+        // sync from sysclock to rtc
+
         Ok(())
     }
 
     /// set the system timezone
-    #[allow(unused_variables)]
     async fn set_timezone(&self, timezone: String, interactive: bool) -> fdo::Result<()> {
         // TODO
         // https://github.com/systemd/systemd/blob/main/src/timedate/timedated.c#L657
+        // check if valid tz (return if not)
+        // check if is current tz (return if true)
+        // check polkit auth
+        self.check_auth("org.freedesktop.timedate1.set-timezone", interactive).await?;
+        // write new localtime symlink
+        // tzset
+        // tell kernel new tz
+        // if local rtc, sync rtc from sysclock
         Ok(())
     }
 
     /// control whether the RTC is in local time or UTC
     #[dbus_interface(name = "SetLocalRTC")]
-    #[allow(unused_variables)]
     async fn set_local_rtc(&self, local_rtc: bool, fix_system: bool, interactive: bool) -> fdo::Result<()> {
         // TODO
         // https://github.com/systemd/systemd/blob/main/src/timedate/timedated.c#L734
+        // if local_rtc matches current state and not fix_system, return
+        let curr = self.local_rtc().await?;
+        if local_rtc == curr && ! fix_system {
+            return Ok(());
+        }
+
+        // check polkit for auth
+        self.check_auth("org.freedesktop.timedate1.set-local-rtc", interactive).await?;
+        // if local_rtc doesn't match, change it
+        if local_rtc != curr {
+            // change value
+            todo!()
+        }
+        // tell kernel the tz
+        // sync clocks
+        // if fix_system, sync system clock from rtc
+        // else sync rtc from system clock
+        // emit prop localrtc changed
         Ok(())
     }
 
@@ -67,29 +106,15 @@ impl TimeDate {
 
     /// returns a list of time zones known on the local system
     async fn list_timezones(&self) -> fdo::Result<Vec<String>> {
-        let fp = Path::new("/usr/share/zoneinfo/zone.tab");
-        if fp.exists() {
-            match read_lines(fp).await {
-                Err(e) => return Err(fdo::Error::Failed(format!("Couldn't get timezone list: {}", e))),
-                Ok(lines) => {
-                    let mut out = vec![];
-                    for ln in lines {
-                        if let Ok(l) = ln {
-                            // if not a comment
-                            if ! l.starts_with('#') {
-                                let mut parts = l.split_whitespace();
-                                // grab the 3rd part of the line
-                                if let Some(tz) = parts.nth(2) {
-                                    out.push(tz.to_string());
-                                }
-                            }
-                        }
-                    }
-                    return Ok(out);
-                }
+        match read_lines("/usr/share/zoneinfo/zone.tab").await {
+            Ok(lines) => {
+                // grab the third word of each non-comment line
+                Ok(lines.filter(|ln| ln.is_ok() && ! ln.as_ref().unwrap().starts_with('#'))
+                        .filter_map(|ln| ln.unwrap().split_whitespace().nth(2).map(str::to_string))
+                        .collect())
             }
+            Err(e) => Err(fdo::Error::Failed(format!("Couldn't get timezone list: {}", e))),
         }
-        Err(fdo::Error::Failed("Couldn't get timezone list: file not found".into()))
     }
 
     /// shows the currently configured time zone
@@ -115,20 +140,16 @@ impl TimeDate {
     async fn local_rtc(&self) -> fdo::Result<bool> {
         // see adjtime_config(5)
         // if /etc/adjtime exists, check 3rd line for "LOCAL" or "UTC"
-        let fp = Path::new("/etc/adjtime");
-        if fp.exists() {
-            match read_lines(fp).await {
-                Err(e) => return Err(fdo::Error::Failed(format!("Couldn't get RTC status: {}", e))),
-                Ok(mut lines) => {
-                    if let Some(Ok(ln)) = lines.nth(2) {
-                        // return true if 3rd line is LOCAL, false if UTC (or otherwise)
-                        return Ok(ln == "LOCAL");
-                    }
+        match read_lines("/etc/adjtime").await {
+            Err(e) => Err(fdo::Error::Failed(format!("Couldn't get RTC status: {}", e))),
+            Ok(mut lines) => {
+                if let Some(Ok(ln)) = lines.nth(2) {
+                    Ok(ln == "LOCAL")
+                } else {
+                    Ok(false)  // assume UTC otherwise
                 }
             }
         }
-        // else assume UTC
-        Ok(false)
     }
 
     /// shows whether a service to perform time synchronization over the network is available
@@ -152,7 +173,7 @@ impl TimeDate {
             false
         } else {
             // consider synced if within NTP_PHASE_LIMIT, relying on STA_UNSYNC isn't always reliable
-            // see include/linux/timex.h and kernel/time/ntp.c for NTP_PHASE_LIMIT
+            // see include/linux/timex.h and kernel/time/ntp.c (NTP_PHASE_LIMIT)
             buf.maxerror < ((MAX_PHASE / NSEC_TO_USEC) << 5)
         }
     }
@@ -161,15 +182,13 @@ impl TimeDate {
     #[dbus_interface(property(emits_changed_signal = "false"), name = "TimeUSec")]
     async fn time_usec(&self) -> fdo::Result<u64> {
         match nix::time::clock_gettime(nix::time::ClockId::CLOCK_REALTIME) {
-            Ok(ts) => Ok(
-                (
-                    (ts.tv_sec() * SEC_TO_USEC) + (ts.tv_nsec() / NSEC_TO_USEC)
-                ).try_into().unwrap_or_default()
-            ),
+            Ok(ts) => Ok((
+                (ts.tv_sec() * SEC_TO_USEC) + (ts.tv_nsec() / NSEC_TO_USEC)
+            ).try_into().unwrap_or_default()),
             Err(e) => match e {
                 Errno::ENOSYS => Err(fdo::Error::NotSupported("clock_gettime not supported on this system".into())),
                 Errno::EINVAL => Err(fdo::Error::Failed("Unable to get current time".into())),
-                _ => Err(fdo::Error::Failed(e.desc().into())),
+                _ => Err(fdo::Error::Failed(format!("Unable to get current time: {}", e.desc()))),
             }
         }
     }
@@ -188,15 +207,39 @@ impl TimeDate {
             Err(e) => Err(fdo::Error::Failed(format!("Couldn't open /dev/rtc: {}", e.desc()))),
         }
     }
+
+    async fn check_auth(&self, action: &str, interactive: bool) -> zbus::fdo::Result<()> {
+        let auth_res = self.auth.check_authorization(
+            &self.subject,
+            action,
+            &std::collections::HashMap::new(),
+            if interactive { CheckAuthorizationFlags::AllowUserInteraction.into() }
+                else { CheckAuthorizationFlags::empty() },
+            ""
+        ).await?;
+
+        if auth_res.is_authorized {
+            Ok(())
+        } else if auth_res.is_challenge {
+            Err(zbus::fdo::Error::AuthFailed("Interactive authentication required".into()))
+        } else {
+            match caps::has_cap(None, caps::CapSet::Effective, caps::Capability::CAP_SYS_TIME) {
+                Ok(true) => Ok(()),
+                Ok(false) | Err(_) => Err(zbus::fdo::Error::AuthFailed("Does not have CAP_SYS_TIME".into()))
+            }
+        }
+    }
 }
 
 #[tokio::main]
-async fn main() -> zbus::Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let conn = Connection::system().await?;
     let timedate = TimeDate {
         tz: std::env::var("TZ").ok(),
         rtc: rtc_open(),
+        auth: AuthorityProxy::new(&conn).await?,
+        subject: Subject::new_for_owner(std::process::id(), None, None)?,
     };
-    let conn = Connection::system().await?;
     conn.object_server().at("/org/freedesktop/timedate1", timedate).await?;
     conn.request_name("org.freedesktop.timedate1").await?;
 
